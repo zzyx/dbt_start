72a275b53224dce7b12cce90e3cd97a488d4daec
m.amurov@gmail.com <m.amurov@gmail.com>
2023-01-30 14:46:41 +0000

    makros added

--- models/staging/stripe/stg_payment.sql
+++ models/staging/stripe/stg_payment.sql
@@ -5,7 +5,7 @@ with payment as (
         ORDERID as order_id,
         PAYMENTMETHOD as payment_method,
         status,
-        amount / 100 as amount,
+        {{ cents_to_dollars('amount', 2) }}  as amount,
         created as created_at
 
    from {{ source('stripe', 'payment') }} 
